681f89f83466ef624ad60df91d0c74de44e8b8b4
Daniel Valencia <dfvalenciar@unal.edu.co>
2023-11-25 17:24:21 -0500

    Cambios en la paginacion

--- Scripts/listaProductos.js
+++ Scripts/listaProductos.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let productos = document.querySelector('.products');
+const productos = document.querySelector('.products');
 
+document.addEventListener('DOMContentLoaded', function() {
     async function fetchProducts(url) {
         try {
             let res = await fetch(url);
@@ -8,23 +8,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
             data.forEach(producto => {
                 // let description = producto.description;
-                let title = producto.title;
-
                 productos.innerHTML += `
-                    <div class="product">
-                        <a href="" class="link-product">
+                    <div class="product" id="product-${producto.id}">
+                        <a href="../index.html" class="link-product">
                             <img src="${producto.image}" alt="${producto.category}" class="product-img">
                             <div class="product-info">
-                                <h2 class="product-title">${title}</h2>
+                                <h2 class="product-title">${producto.title}</h2>
                                 <h4 class="product-category">${producto.category}</h4>
                                 <div class="product-price-container">
                                     <h3 class="product-price">$${producto.price}</h3>
-                                    <a href="#!" data-productId="${producto.id}" class="add-to-cart"><ion-icon name="cart-outline"></ion-icon></a>
+                                    <button>
+                                        <a href="#!" data-productId="${producto.id}" class="add-to-cart"><ion-icon name="cart-outline"></ion-icon></a>
+                                    </button>
                                 </div>
                             </div>
                         </a>
                     </div>`;  
                     // <p class="product-description">${description.length > 80 ? description.substring(0, 80).concat(' ... más') : description}</p>
+
+                let productoContainer = document.querySelectorAll('.product');
+
+                productoContainer.forEach(container => {
+                    container.addEventListener('click', function () {
+                        const link = container.querySelector('.link-product');
+                        const productUrl = link.getAttribute('href');
+
+                        // Redirige a la URL deseada
+                        window.location.href = productUrl;
+                    });
+                });
             });
         } catch (error) {
             console.log(error);
